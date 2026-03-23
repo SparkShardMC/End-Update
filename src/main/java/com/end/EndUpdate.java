c731e7de83d30c4ff347914a12494f9de6dbb154
ScriptBox <zaki.coding0119@gmail.com>
2026-03-23 09:56:49 -0700

    Update EndUpdate.java

--- src/main/java/com/end/EndUpdate.java
+++ src/main/java/com/end/EndUpdate.java
@@ -1,23 +1,38 @@
 package com.end;
 
+import com.end.block.CrystaledObsidianBlock;
 import com.end.block.VoidGlassBlock;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Blocks;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class EndUpdate implements ModInitializer {
     public static final String MOD_ID = "endupdate";
+
     public static final VoidGlassBlock VOID_GLASS = new VoidGlassBlock(AbstractBlock.Settings.copy(Blocks.GLASS));
+    public static final CrystaledObsidianBlock CRYSTALED_OBSIDIAN = new CrystaledObsidianBlock(AbstractBlock.Settings.copy(Blocks.OBSIDIAN));
 
     @Override
     public void onInitialize() {
-        Identifier id = Identifier.of(MOD_ID, "void_glass");
-        Registry.register(Registries.BLOCK, id, VOID_GLASS);
-        Registry.register(Registries.ITEM, id, new BlockItem(VOID_GLASS, new Item.Settings()));
+        registerBlock("void_glass", VOID_GLASS);
+        registerBlock("crystaled_obsidian", CRYSTALED_OBSIDIAN);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(content -> {
+            content.add(VOID_GLASS);
+            content.add(CRYSTALED_OBSIDIAN);
+        });
+    }
+
+    private void registerBlock(String name, net.minecraft.block.Block block) {
+        Identifier id = Identifier.of(MOD_ID, name);
+        Registry.register(Registries.BLOCK, id, block);
+        Registry.register(Registries.ITEM, id, new BlockItem(block, new Item.Settings()));
     }
 }
